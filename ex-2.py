7fb98fbe282b7d2506b3622d2f619bd5a1cadac1
GuiilhermeLuis <guilhermedejesus320@gmail.com>
2025-03-13 19:39:10 -0300

    Update ex-2.py
    2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.

--- ex-2.py
+++ ex-2.py
@@ -1,4 +1,3 @@
-2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
-NOME: Guilherme luis
-ESTADO: Rio de Janeiro
-MUNICÍPIO: Rio de Janeiro
+print("NOME: Guilherme Luis")
+print("ESTADO: Rio de Janeiro")
+print("MUNICÍPIO: Rio de Janeiro")
