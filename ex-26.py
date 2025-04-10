93d7cbf69159a7de1e1ffede3377b13aeba8665d
GuiilhermeLuis <guilhermedejesus320@gmail.com>
2025-04-10 19:59:44 -0300

    Create ex-26.py
    26) Ler 5 números digitados pelo usuário e armazenar em um vetor. Depois, imprime os números informados.

---
+++ ex-26.py
@@ -0,0 +1,12 @@
+vetor = []
+# Aqui, criamos uma lista vazia chamada vetor. Ele guarda os 5 numeros digitados pelo usuario
+for i in range(5):
+# O laço for vai repetir 5 vezes(de i = 0 até i = 4)
+    numero = int(input("Digite o {i+1} numero: "))
+# O {i+1} é só para mostrar a contagem de forma amigável (de 1 a 5)
+    vetor.append(numero)
+# Adiciona o número digitado no final da lista vetor
+print("Os numeros informados foram:")
+for numero in vetor:
+# Pecorre a lista vetor elemento por elemento
+    print(numero)
