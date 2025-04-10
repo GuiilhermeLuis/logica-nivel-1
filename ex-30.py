fdb7b787fa22609cbed33fad5700775a65f64889
GuiilhermeLuis <guilhermedejesus320@gmail.com>
2025-04-10 20:36:34 -0300

    Create ex-30.py
    30) Contar quantas vezes um número aparece em um vetor

---
+++ ex-30.py
@@ -0,0 +1,8 @@
+# Lista com alguns numeros
+vetor = [7, 9, 5, 7, 3, 5, 7, 9, 10]
+
+# Numero que queremos contar
+x = 7
+
+# Contando e imprimindo
+print("O numero", x, "aparece", vetor.count(x), "vezes")
